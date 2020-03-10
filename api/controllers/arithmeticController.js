173be5edfcd095e7e4e90697859c821c44b0b60d
jayzee-c <62023597+jayzee-c@users.noreply.github.com>
2020-03-10 08:26:42 -0700

    Redid
    testte

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -10,6 +10,7 @@ exports.calculate = function(req, res) {
     res.json({ error: err.message });
   });
 
+  // redid plus operand
   var operations = {
     'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
